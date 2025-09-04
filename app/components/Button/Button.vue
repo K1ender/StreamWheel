0c18129134aabb3fc23c91f1bb245548fec67420
K1ender <146767945+K1ender@users.noreply.github.com>
2025-09-04 19:41:11 +0300

    feat(ui): add button component

---
+++ app/components/Button/Button.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { cva } from 'class-variance-authority';
+
+const buttonVariants = cva(
+  ["flex", "cursor-pointer", "transition-colors", "ease-out", "gap-2"],
+  {
+    variants: {
+      intent: {
+        primary: "bg-purple-500 hover:bg-purple-600",
+        secondary: "1",
+      },
+      size: {
+        sm: "1",
+        md: "px-3.5 py-2 rounded-md",
+        lg: "1",
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+      size: "md",
+    },
+  }
+);
+
+// FIXME: Some problems with types, make issue?
+const { intent = "primary", size = "md" } = defineProps<{
+  intent?: "primary" | "secondary";
+  size?: "sm" | "md" | "lg";
+}>();
+</script>
+
+<template>
+  <button :class="buttonVariants({ intent, size })">
+    <slot name="icon"></slot>
+    <slot />
+  </button>
+</template>
+
+<style></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,8 +5,13 @@
 <template>
   <div class="h-screen bg-neutral-950 text-white flex items-center justify-center flex-col gap-2">
     <h1 class="text-3xl fond-bold">StreamWheel</h1>
-    <button
-      class="flex bg-purple-500 px-3.5 py-2 rounded-md cursor-pointer hover:bg-purple-600 transition-colors ease-out">Login
-      with Twitch</button>
+    <Button>
+      <template #icon>
+        <img src="/glitch_flat_ice.svg" width="16" />
+      </template>
+      <template #default>
+        Login with Twitch
+      </template>
+    </Button>
   </div>
 </template>
